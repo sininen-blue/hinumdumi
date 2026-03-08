c6bfdf47714e6dbe737346178ee634f5fbd5fe91
Neil Alaan <alaanneil@gmail.com>
2026-03-08 23:07:42 +0800

    dialogue test

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 200
+@export var speed: float = 100
 @export var accel: float = 20
 @export var decel: float = 20
 
--- scripts/state.gd
+++ scripts/state.gd
@@ -1,5 +1,6 @@
 extends Node
 
 var has_met_frog: bool = false
+var has_looked_at_picture: bool = false
 
 var inventory: Dictionary = {}
