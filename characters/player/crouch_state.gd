a1beb70926776d3e319f22effe164fa1bc37d3f2
Neil Alaan <alaanneil@gmail.com>
2026-04-02 00:57:28 +0800

    crouch bugfix

--- characters/player/crouch_state.gd
+++ characters/player/crouch_state.gd
@@ -7,7 +7,7 @@ extends State
 @export var crouch_shape: Shape3D
 
 var previous_shape: Shape3D
-var previous_height: float
+var previous_y_position: float
 var previous_head_height: float
 var queue_uncrouch: bool = false
 
@@ -20,18 +20,18 @@ var queue_uncrouch: bool = false
 func enter() -> void:
 	queue_uncrouch = false
 	previous_shape = collision_shape.shape
-	previous_height = collision_shape.position.y
+	previous_y_position = collision_shape.position.y
 	previous_head_height = head.position.y
 	
 	collision_shape.shape = crouch_shape
-	collision_shape.position.y = (previous_shape.height - crouch_shape.height) / 2
+	collision_shape.position.y = previous_y_position - (previous_shape.height - crouch_shape.height) / 2
 	head.position.y =  (previous_shape.height - crouch_shape.height) / 2
 
 
 
 func exit() -> void:
 	collision_shape.shape = previous_shape
-	collision_shape.position.y = previous_height
+	collision_shape.position.y = previous_y_position
 	head.position.y = previous_head_height
 
 
