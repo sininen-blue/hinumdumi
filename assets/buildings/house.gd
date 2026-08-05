8c5131858950a55143c8ea2b470847f23362b5bd
Neil Alaan <alaanneil@gmail.com>
2026-08-05 20:06:36 +0800

    new everything

---
+++ assets/buildings/house.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+const INSIDE_POSITION: Vector3 = Vector3.ZERO
+const OUTSIDE_POSITION: Vector3 = Vector3.ZERO
+
+
+func _on_exit_area_body_entered(body: Node3D) -> void:
+	pass
+
+
+func _on_enter_area_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
