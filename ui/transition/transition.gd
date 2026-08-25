329b454d015961458166c7aa51ec7cd0ff7fe8f7
Neil Alaan <alaanneil@gmail.com>
2026-08-25 10:36:14 +0800

    transition changes

--- ui/transition/transition.gd
+++ ui/transition/transition.gd
@@ -13,9 +13,30 @@ signal done_transition
 
 func _ready() -> void:
 	timer.start()
-	title_label.text = "Night %d" % night_number
+	title_label.text = "Night %d" % _romanize(night_number)
 	missing_label.text = "There are %d missing children" % missing_number
 
 
 func _on_timer_timeout() -> void:
 	done_transition.emit()
+
+
+func _romanize(n: int) -> String:
+	var symbols: Dictionary[int, String] = {
+		1: "I",
+		4: "IV",
+		5: "V",
+		9: "IX",
+		10: "X",
+	}
+	var nums: Array[int] = symbols.keys()
+	nums.sort()
+	
+	var roman: String = ""
+	while n > 0:
+		for i: int in nums:
+			if n > i:
+				n -= i
+				roman += symbols[i]
+		
+	return roman
