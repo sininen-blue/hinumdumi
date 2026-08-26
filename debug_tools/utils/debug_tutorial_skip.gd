2af80aedfaff73fe216586fa2fd8413d73d428ad
Neil Alaan <alaanneil@gmail.com>
2026-08-26 21:45:43 +0800

    sound stuff

---
+++ debug_tools/utils/debug_tutorial_skip.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+
+const DEMO_LEVEL_1 = preload("uid://bwd570eivx0mj")
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is not Player:
+		return
+	
+	SceneManager.change_scene(DEMO_LEVEL_1, true, 1, 2)
