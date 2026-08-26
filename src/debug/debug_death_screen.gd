01ae37a492b5f18a403dfe886fc97f07ef2098a4
Neil Alaan <alaanneil@gmail.com>
2026-08-26 13:55:45 +0800

    death scenes

--- src/debug/debug_death_screen.gd
+++ src/debug/debug_death_screen.gd
@@ -1,15 +1,22 @@
 extends Control
 
-
-@onready var button: Button = $Button
+@onready var restart_game: Button = $RestartGame
+@onready var restart_level: Button = $RestartLevel
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
-func _on_button_pressed() -> void:
-	button.disabled = true
+func _on_restart_game_pressed() -> void:
+	restart_game.disabled = true
+	PlayerInventory.reset()
+	PlayerStates.reset()
+	SceneManager.change_scene(load("res://ui/main_menu/main_menu.tscn"))
+
+
+func _on_restart_level_pressed() -> void:
+	restart_level.disabled = true
 	PlayerInventory.reset()
 	PlayerStates.reset()
 	SceneManager.change_scene(load("res://levels/Demo/demo_level_1.tscn"))
